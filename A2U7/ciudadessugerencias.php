9cbcf2a6d3c73cb35b87d19329c52212c25fae57
PtaZeta <josemanuel.rodriguez.lorenzo@iesdonana.org>
2025-02-14 10:55:04 +0100

    vubjyhg

--- A2U7/ciudadessugerencias.php
+++ A2U7/ciudadessugerencias.php
@@ -1,14 +1,11 @@
 <?php
-// Lista de ciudades
 $a = ["Gijón", "Gibraltar", "Granada", "Guadalajara", "Girona", 
       "Gante", "Génova", "Glasgow", "Gdansk", "Graz"];
 
-// Obtener el parámetro 'q' de la URL
 $q = isset($_REQUEST["q"]) ? $_REQUEST["q"] : "";
 
 $hint = "";
 
-// Buscar coincidencias si 'q' no está vacío
 if ($q !== "") {
     $q = strtolower($q);
     $len = strlen($q);
@@ -19,6 +16,5 @@ if ($q !== "") {
     }
 }
 
-// Si no hay coincidencias, devolver "No hay sugerencias"
 echo $hint === "" ? "No hay sugerencias" : $hint;
 ?>
